b510c7497f1957a4ece6fe430102b60c10dbb8d1
Aline Silva <aline_silva93@outlook.com.br>
2017-10-13 00:50:55 -0300

    Automatizado entrada de dados com Selenium webdriver

--- src/test/java/simple/example1.java
+++ src/test/java/simple/example1.java
@@ -1,7 +1,7 @@
-package test.java.simple;
+package simple;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class example1 {
@@ -13,8 +13,14 @@ public class example1 {
 
 	driver.navigate().to("https://twitter.com/ProjetoTesteS2B");
 	
-	driver.quit();
-	
+	// Procura o elemento do form de login e salva na variavel
+	WebElement loginForm = driver.findElement(By.xpath("//ul[contains(@id, 'session')]//form[contains(@class, 'LoginForm')]"));
+    // Insere o usuário
+	loginForm.findElement(By.xpath("div[1]/input")).sendKeys("ProjetoTesteS2B");
+	// insere a senha
+	loginForm.findElement(By.xpath("div[2]/input")).sendKeys("Aline64034240");
+	// Clica no botão login
+	loginForm.findElement(By.xpath("input[1]")).click();	
 	
 	}
 
